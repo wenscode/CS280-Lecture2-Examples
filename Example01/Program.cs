87fe664afd411138549c545a4262c40e14fd8b0a
wenscode <wenschair@gmail.com>
2017-03-10 12:25:25 +0800

    簡單登入程式

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Example01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.Write("請輸入名字");
+            string name = Console.ReadLine();  //創造一個字串變數name，讓使用者輸入字，然後存成文字變數
+            Console.Write("請輸入密碼");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Hi, {0}歡迎登入", name);  //{0}就用name這個變數去置換
         }
     }
 }
